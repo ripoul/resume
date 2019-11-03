e5445eaf35fbb4301ef5d0ae2970e819348702b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 14:54:01 +0000

    Deploy ripoul/resume to github.com/ripoul/resume.git:gh-pages

---
+++ precache-manifest.42618b435ca625ecdf6d92459b085c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b415537e938d83874b8be7db4b67b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc88ce9e1b4dbb5247e4",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0255d43d6c95f4ba3646",
+    "url": "/static/js/2.833ff8e5.chunk.js"
+  },
+  {
+    "revision": "cc88ce9e1b4dbb5247e4",
+    "url": "/static/js/main.86feba2e.chunk.js"
+  },
+  {
+    "revision": "bb6e324978c6b93739f5",
+    "url": "/static/js/runtime-main.e7b7af1d.js"
+  }
+]);
