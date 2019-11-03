99e5d37fe6fb0fe72eec72792e7ea02ad00428d8
Jules LE BRIS <jls.lebris@gmail.com>
2019-11-03 19:23:16 +0100

    :bug: fix #2 add missing space in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,8 +18,7 @@ class Footer extends Component {
             <ul className="copyright">
               <li>&copy; Copyright 2019 Jules LE BRIS</li>
               <li>
-                Design by
-                <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a>
+                Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a>
               </li>
             </ul>
 
