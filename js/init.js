0a54b4adccdfdcd3b5bbfcb2351dffc9ad1cc817
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 13:16:43 +0000

    Deploy ripoul/ripoul.github.io to github.com/ripoul/ripoul.github.io.git:gh-pages

---
+++ js/init.js
@@ -0,0 +1,125 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+jQuery(document).ready(($) => {
+  /*----------------------------------------------------*/
+  /* FitText Settings
+------------------------------------------------------ */
+
+  setTimeout(() => {
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	 }, 100);
+
+
+  /*----------------------------------------------------*/
+  /* Smooth Scrolling
+------------------------------------------------------ */
+
+  $('.smoothscroll').on('click', function (e) {
+	    e.preventDefault();
+
+	    const target = this.hash;
+	    const $target = $(target);
+
+	    $('html, body').stop().animate({
+	        scrollTop: $target.offset().top,
+	    }, 800, 'swing', () => {
+	        window.location.hash = target;
+	    });
+  });
+
+
+  /*----------------------------------------------------*/
+  /* Highlight the current section in the navigation bar
+------------------------------------------------------*/
+
+  const sections = $('section');
+  const navigation_links = $('#nav-wrap a');
+
+  sections.waypoint({
+
+    handler(event, direction) {
+		   let active_section;
+
+      active_section = $(this);
+      if (direction === 'up') active_section = active_section.prev();
+
+      const active_link = $(`#nav-wrap a[href="#${active_section.attr('id')}"]`);
+
+      navigation_links.parent().removeClass('current');
+      active_link.parent().addClass('current');
+    },
+    offset: '35%',
+
+  });
+
+
+  /*----------------------------------------------------*/
+  /*	Make sure that #header-background-image height is
+/* equal to the browser height.
+------------------------------------------------------ */
+
+  $('header').css({ height: $(window).height() });
+  $(window).on('resize', () => {
+    $('header').css({ height: $(window).height() });
+    $('body').css({ width: $(window).width() });
+  });
+
+
+  /*----------------------------------------------------*/
+  /*	Fade In/Out Primary Navigation
+------------------------------------------------------*/
+
+  $(window).on('scroll', () => {
+    const h = $('header').height();
+    const y = $(window).scrollTop();
+    const nav = $('#nav-wrap');
+
+	   if ((y > h * 0.20) && (y < h) && ($(window).outerWidth() > 768)) {
+	      nav.fadeOut('fast');
+	   } else if (y < h * 0.20) {
+      nav.removeClass('opaque').fadeIn('fast');
+    } else {
+      nav.addClass('opaque').fadeIn('fast');
+    }
+  });
+
+
+  /*----------------------------------------------------*/
+  /*	Modal Popup
+------------------------------------------------------*/
+
+  $('.item-wrap a').magnificPopup({
+
+    type: 'inline',
+    fixedContentPos: false,
+    removalDelay: 200,
+    showCloseBtn: false,
+    mainClass: 'mfp-fade',
+
+  });
+
+  $(document).on('click', '.popup-modal-dismiss', (e) => {
+    		e.preventDefault();
+    		$.magnificPopup.close();
+  });
+
+
+  /*----------------------------------------------------*/
+  /*	Flexslider
+/*----------------------------------------------------*/
+  $('.flexslider').flexslider({
+    namespace: 'flex-',
+    controlsContainer: '.flex-container',
+    animation: 'slide',
+    controlNav: true,
+    directionNav: false,
+    smoothHeight: true,
+    slideshowSpeed: 7000,
+    animationSpeed: 600,
+    randomize: false,
+  });
+});
